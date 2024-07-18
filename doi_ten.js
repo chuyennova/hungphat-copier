9d5c182e38134a5e394e11d73f2fe3637ecd237a
chuyennova <chuotlun1988@gmail.com>
2024-07-18 10:45:07 -0500

    Create doi_ten.js

---
+++ doi_ten.js
@@ -0,0 +1,27 @@
+				 <b:if cond='data:blog.searchLabel == "tin-tuc"'> Tin Tức
+                 <b:elseif cond='data:view.search.label == "thue-may-photocopy"' /> Thuê Máy Photocopy
+				 <b:elseif cond='data:view.search.label == "ban-may-photocopy"' /> Bán Máy Photocopy
+				 <b:elseif cond='data:view.search.label == "linh-kien-may-photocopy"' /> Linh Kiện Máy Photocopy
+                 <b:else />
+                 <data:view.search.label/>
+                 </b:if>
+				 
+				 <b:if cond='data:blog.searchLabel == "tin-tuc"'> Tin Tức
+                 <b:elseif cond='data:view.search.label == "thue-may-photocopy"' /> Thuê Máy Photocopy
+				 <b:elseif cond='data:view.search.label == "ban-may-photocopy"' /> Bán Máy Photocopy
+				 <b:elseif cond='data:view.search.label == "linh-kien-may-photocopy"' /> Linh Kiện Máy Photocopy
+                 <b:else />
+                 <data:view.search.label/>
+                 </b:if>
+				 
+				 
+				 
+			
+				 
+				 <b:if cond='data:post.labels.last.name == "tin-tuc"'> Tin Tức
+                 <b:elseif cond='data:post.labels.last.name == "thue-may-photocopy"' /> Thuê Máy Photocopy
+				 <b:elseif cond='data:post.labels.last.name == "ban-may-photocopy"' /> Bán Máy Photocopy
+				 <b:elseif cond='data:post.labels.last.name == "linh-kien-may-photocopy"' /> Linh Kiện Máy Photocopy	
+				 <b:else />
+				 <data:post.labels.last.name/>
+				 </b:if>
